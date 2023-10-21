028c12c4f8cbc3c03bdc46538b5645f4a3a75018
Lau-r-a <103428878+Lau-r-a@users.noreply.github.com>
2023-10-21 13:07:25 +0200

    refactor project

---
+++ src/controller/bot.js
@@ -0,0 +1,31 @@
+import {
+  MatrixClient,
+  SimpleFsStorageProvider,
+  AutojoinRoomsMixin,
+} from "matrix-bot-sdk";
+
+export function initBot(homeserverUrl, accessToken, storageFile) {
+  const storage = new SimpleFsStorageProvider(storageFile);
+  const client = new MatrixClient(homeserverUrl, accessToken, storage);
+
+  AutojoinRoomsMixin.setupOnClient(client);
+
+  client.start().then(() => console.log("Client started!"));
+
+  client.on("room.message", (roomId, event) => {
+    if (!event["content"]) return;
+    const sender = event["sender"];
+    const body = event["content"]["body"];
+    console.log(`${roomId}: ${sender} says '${body}`);
+
+    if (body.startsWith("!echo")) {
+      const replyText = body.substring("!echo".length).trim();
+      client.sendMessage(roomId, {
+        msgtype: "m.notice",
+        body: replyText,
+      });
+    }
+  });
+
+  return client;
+}
--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
 import "dotenv/config";
-import {
-    MatrixClient,
-    SimpleFsStorageProvider,
-    AutojoinRoomsMixin,
-} from "matrix-bot-sdk";
+import { initBot } from "./controller/bot.js";
 
-const homeserverUrl = process.env.MATRIX_HOMESERVER;
-const accessToken = process.env.MATRIX_TOKEN;
-
-const storage = new SimpleFsStorageProvider("bot.json");
-
-const client = new MatrixClient(homeserverUrl, accessToken, storage);
-AutojoinRoomsMixin.setupOnClient(client);
-
-client.start().then(() => console.log("Client started!"));
-
-client.on("room.message", (roomId, event) => {
-    if (! event["content"]) return;
-    const sender = event["sender"];
-    const body = event["content"]["body"];
-    console.log(`${roomId}: ${sender} says '${body}`);
-});
+const client = initBot(
+  process.env.MATRIX_HOMESERVER,
+  process.env.MATRIX_TOKEN,
+  process.env.BOT_STORAGE
+);
