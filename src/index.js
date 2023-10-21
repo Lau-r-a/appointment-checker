a955fd073e448c22afbc7c569d7f5e6e81348690
xenya52 <108221381+xenya52@users.noreply.github.com>
2023-10-21 12:39:35 +0200

    add basic bot functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import "dotenv/config";
+import {
+    MatrixClient,
+    SimpleFsStorageProvider,
+    AutojoinRoomsMixin,
+} from "matrix-bot-sdk";
 
-console.log(process.env.MATRIX_ROOM);
+const homeserverUrl = process.env.MATRIX_HOMESERVER;
+const accessToken = process.env.MATRIX_TOKEN;
 
-console.log("Hello World");
+const storage = new SimpleFsStorageProvider("bot.json");
+
+const client = new MatrixClient(homeserverUrl, accessToken, storage);
+AutojoinRoomsMixin.setupOnClient(client);
+
+client.start().then(() => console.log("Client started!"));
+
+client.on("room.message", (roomId, event) => {
+    if (! event["content"]) return;
+    const sender = event["sender"];
+    const body = event["content"]["body"];
+    console.log(`${roomId}: ${sender} says '${body}`);
+});
