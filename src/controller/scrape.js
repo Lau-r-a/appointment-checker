7840e4478fb41e6259e6187a789caf84eb82b491
Lau-r-a <103428878+Lau-r-a@users.noreply.github.com>
2023-10-22 09:34:56 +0200

    refactoring for further testing

--- src/controller/scrape.js
+++ src/controller/scrape.js
@@ -1,48 +1,47 @@
 import axios from "axios";
 
-function setDate(multiplicator) {
-
-  let date = new Date(process.env.START_DATE)
+function setDate(start_date, multiplicator) {
+  let date = new Date(start_date);
   date.setHours(24 * multiplicator * 15);
 
   let year = new Intl.DateTimeFormat("en", { year: "numeric" }).format(date);
   let month = new Intl.DateTimeFormat("en", { month: "2-digit" }).format(date);
   let day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(date);
 
-
-  return {params: {start_date:`${year}-${month}-${day}`}}
+  return { params: { start_date: `${year}-${month}-${day}` } };
 }
 
-export async function doOnAppointmentAvailable(url, callback) {
-  for (let i = 0; i < process.env.LOOKAHEAD; i++) {
-    let option = setDate(i);
-    
-    axios
-    .get(url, option)
-    .then(function (response) {
-      if (response.data.total != undefined && response.data.total == 0) {
-        if (
-          response.data.reason != undefined &&
-          response.data.message != undefined
-        ) {
-          console.log(
-            "No appointment available with option " + JSON.stringify(option) + " with reason " +
-              response.data.reason +
-              " and message: " +
-              response.data.message
-          );
-          callback(false);
-        }
-      } else {
-        callback(true);
-      }
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .finally(function () {
-      // always executed
-    });
+async function availableWithOption(url, option) {
+  let response = await axios.get(url, option);
+
+  if (response.data.total != undefined && response.data.total == 0) {
+    if (
+      response.data.reason != undefined &&
+      response.data.message != undefined
+    ) {
+      console.log(
+        "No appointment available with option " +
+          JSON.stringify(option) +
+          " with reason " +
+          response.data.reason +
+          " and message: " +
+          response.data.message
+      );
+      return false;
+    }
+  } else {
+    console.log("Appointment available with option " + JSON.stringify(option));
+    return true;
   }
+}
+
+export async function isAvailable(url, start_date, lookahead) {
+  let isAvailable = false;
+  for (let i = 0; i < lookahead; i++) {
+    let option = setDate(start_date, i);
+    if (availableWithOption(url, option)) {
+      isAvailable = true;
+    }
+  }
+  return isAvailable;
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import "dotenv/config";
 import { initBot, sendMessage } from "./controller/bot.js";
-import { doOnAppointmentAvailable } from "./controller/scrape.js";
+import { isAvailable } from "./controller/scrape.js";
+
+function sendMessageOnAvailability() {
+  if (
+    isAvailable(
+      process.env.SCRAPE_SITE,
+      process.env.START_DATE,
+      process.env.LOOKAHEAD
+    )
+  ) {
+    console.log("Sending message: Appointment is available");
+    sendMessage(client, process.env.MATRIX_ROOM, "Appointment is available");
+  }
+}
 
 const client = initBot(
   process.env.MATRIX_HOMESERVER,
@@ -8,12 +21,5 @@ const client = initBot(
   process.env.BOT_STORAGE
 );
 
-setInterval(() => {
-  doOnAppointmentAvailable(process.env.SCRAPE_SITE, (isAvailable) => {
-    //write bot message
-    console.log("Appointment is available: " + isAvailable);
-    if (isAvailable) {
-      sendMessage(client, process.env.MATRIX_ROOM, "Appointment is available");
-    }
-  });
-}, 1000 * 60 * 5);
+setInterval(sendMessageOnAvailability, 1000 * 60 * 5);
+sendMessageOnAvailability();
