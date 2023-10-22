ad0707e730393ed3ccb89587128c6b8b80c20431
xenya52 <108221381+xenya52@users.noreply.github.com>
2023-10-22 08:57:26 +0200

    implement the time span

--- src/controller/scrape.js
+++ src/controller/scrape.js
@@ -1,8 +1,24 @@
 import axios from "axios";
 
+function setDate(multiplicator) {
+
+  let date = new Date(process.env.START_DATE)
+  date.setHours(24 * multiplicator * 15);
+
+  let year = new Intl.DateTimeFormat("en", { year: "numeric" }).format(date);
+  let month = new Intl.DateTimeFormat("en", { month: "2-digit" }).format(date);
+  let day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(date);
+
+
+  return {params: {start_date:`${year}-${month}-${day}`}}
+}
+
 export async function doOnAppointmentAvailable(url, callback) {
-  axios
-    .get(url)
+  for (let i = 0; i < process.env.LOOKAHEAD; i++) {
+    let option = setDate(i);
+    
+    axios
+    .get(url, option)
     .then(function (response) {
       if (response.data.total != undefined && response.data.total == 0) {
         if (
@@ -10,7 +26,7 @@ export async function doOnAppointmentAvailable(url, callback) {
           response.data.message != undefined
         ) {
           console.log(
-            "No appointment available with reason " +
+            "No appointment available with option " + JSON.stringify(option) + " with reason " +
               response.data.reason +
               " and message: " +
               response.data.message
@@ -28,4 +44,5 @@ export async function doOnAppointmentAvailable(url, callback) {
     .finally(function () {
       // always executed
     });
+  }
 }
